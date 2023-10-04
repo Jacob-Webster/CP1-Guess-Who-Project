6bc2ae0d7798e16baecb701b335c28a49703244c
Jacob Webster <146301645+Jacob-Webster@users.noreply.github.com>
2023-10-04 08:58:36 -0400

    First Submission

---
+++ Guess Who/Characteristics.py
@@ -0,0 +1,5 @@
+Hair_Color_Options = ["Red", "Black", "White", "Brown", "Blonde"]
+
+Characteristics_Dict = {
+    "Hair_Color": Hair_Color_Options 
+}
---
+++ Guess Who/Computer.py
@@ -0,0 +1,6 @@
+def Easy_Mode():
+    print("Now playing on easy mode!")
+def Normal_Mode():
+    print("Now playing on normal mode!")
+def Hard_Mode():
+    print("Now playing on hard mode!")
